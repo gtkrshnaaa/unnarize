45034c91bd9bebd06b3268beab8b13622bf0ebae
gtkrshnaaa <gtkrshnaaa@gmail.com>
2025-12-29 01:04:42 +0700

    Add ucoreSystem header file

---
+++ corelib/include/ucore_system.h
@@ -0,0 +1,9 @@
+#ifndef UCORE_SYSTEM_H
+#define UCORE_SYSTEM_H
+
+#include "vm.h"
+
+// Register the ucoreSystem native library into the VM
+void registerUCoreSystem(VM* vm);
+
+#endif // UCORE_SYSTEM_H
